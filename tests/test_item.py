bcf993051a8923b7356e1c1026a412542baf1ad2
DaniilKokorev <kokorevdaniil@mail.ru>
2024-01-23 22:03:53 +0700

    initial

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,7 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+
+if __name__ == '__main__':
+
+    assert item1.calculate_total_price == 200000
