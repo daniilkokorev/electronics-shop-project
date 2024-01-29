fcc91be7e600aa235a73b8157559b4e928389942
DaniilKokorev <kokorevdaniil@mail.ru>
2024-01-29 21:36:20 +0700

    dander

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,8 +15,8 @@ def test_calculate_total_price(item1):
 
 def test_apply_discount(item1):
     """Тест проверяет стоимоть товара со скидкой"""
-    assert 10000 * item1.pay_rate == item1.price
-    assert item1.price == 10000
+    item1.pay_rate = 0.8
+    assert item1.price * item1.pay_rate == 8000.0
 
 
 def test_instantiate_from_csv(item1):
@@ -27,6 +27,7 @@ def test_instantiate_from_csv(item1):
 
 def test_name(item1):
     """Тест проверяет длину наименования товара не более 10 символов"""
+    item1.name = "Смартфон"
     assert item1.name == "Смартфон"
     item1.name = "СуперСмартфон"
     assert item1.name == "СуперСмарт"
@@ -37,3 +38,11 @@ def test_string_to_number(item1):
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+
+def test_repr_metod(item1):
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str_metod(item1):
+    assert str(item1) == 'Смартфон'
